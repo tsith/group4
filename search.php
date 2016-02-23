d4572036c121ac1dd965e993a54041548d11ab8b
ki3rz <kieran_mitchell@outlook.com>
2016-02-23 20:17:20 +0000

    Update search.php
    - implemented removeCommonWords()
    - implemented setVal()
    - added variable commonWords 
    - improved code elegance

--- search.php
+++ search.php
@@ -10,27 +10,31 @@
 <?php
 
 include "functions.php";
-
-
 $con = connect('csmysql.cs.cf.ac.uk', 'c1433846', 'udruc3', 'c1433846');
+
+$commonWords = 'and,the';
+
+$title = removeCommonWords($commonWords, setVal('title'));
+$author = removeCommonWords($commonWords, setVal('author'));
+$publicationYear = removeCommonWords($commonWords, setVal('year'));
+$citationsMin = removeCommonWords($commonWords, setVal('citationsMin'));
+$citationsMax = removeCommonWords($commonWords, setVal('citationsMax'));
 
 $query = chooseQuery();
 
+//echo $query;
 
 $r = mysqli_query($con, $query, MYSQLI_STORE_RESULT)
     or die("Failed to connect: " . mysqli_error($con));
-
 
 while($row = mysqli_fetch_array($r)){
 	echo "<tr>";
 	echo "<td><br>".$row['Authors']."</td>";
 	echo "<td>".$row['Title']."</td>";
     echo "<td><a href=".$row['ArticleURL'].">".$row['ArticleURL']."</a></td>";
-
 }
 
 closeConnection($con);
-
 
 ?>
 </html>
