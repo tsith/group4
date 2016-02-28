abbfbcc8670f3e4cf6a049676dc1b2094cf58836
ChrisHutchings18 <chris-alex@hotmail.co.uk>
2016-02-28 17:34:45 +0000

    Update javaScripts.js

--- javaScripts.js
+++ javaScripts.js
@@ -11,7 +11,7 @@ document.getElementById('abc').style.display = "none";
 function checkTextField(){
 	var content = "";
 	var correctInput = new RegExp("^[a-z A-Z ]+(\s[a-z A-Z ]+)?$");
-	var nameCheck = correctInput.exec(searchBox.titleOfPaper.value);
+	var nameCheck = correctInput.exec(searchBox.title.value);
 
 	if (!nameCheck){
       content += "Your search cannot contain digits, special characters or be empty \n\n";}
@@ -28,5 +28,55 @@ function checkAdditionalSearch(){
 		alert("You must enter some information");
 		form = document.getElementById("form");
 		form.action = "MainPage.php";}
-	}
+
+	var content = "";
+	var correctInputs = new RegExp("^[a-z A-Z ]+(\s[a-z A-Z ]+)?$");
+	var keywordInput = new RegExp("^[a-z A-Z ,\,]+(\s[a-z A-Z]+)?$");
+	var numberCheck = new RegExp("^\\d{4}$");
+
+	var authorCheck = correctInputs.exec(additionalBox.author.value);
+
+	if (!authorCheck){
+		content += "The author field can only contain letters \n\n";}
+
+    var titleCheck = correctInputs.exec(additionalBox.title.value);
+
+    if (!titleCheck){
+    	content += "The title field can only contain letters \n\n";}
+
+    var keywordsCheck = keywordInput.exec(additionalBox.keywords.value);
+
+	if (!keywordsCheck){
+		content += "The keywords field can only contain letters and commas \n\n";}
+
+    var publisherCheck = correctInputs.exec(additionalBox.publisher.value);
+
+	if (!publisherCheck){
+		content += "The publisher field can only contain letters \n\n";}
+
+	var yearCheck = numberCheck.exec(additionalBox.Year.value);
+
+	if (!yearCheck){
+		content += "The year field can only contain 4 digits \n\n";}
+
+	var citationMinCheck = numberCheck.exec(additionalBox.citationsMin.value);
+
+	if (!citationMinCheck){
+		content += "The citation min field can only contain digits \n\n";}
+
+	var citationMaxCheck = numberCheck.exec(additionalBox.citationsMax.value);
+
+	if (!citationMaxCheck){
+		content += "The citation max field can only contain digits \n\n";}
+
+
+    if(!content =="") //if content is not empty
+      {alert(content); 
+      	form = document.getElementById("form");
+		form.action = "MainPage.php";
+
+
+
+	}}
+
 
