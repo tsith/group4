87fac63a3dfa753b3428bde7638e21d1564d9b69
ChrisHutchings18 <chris-alex@hotmail.co.uk>
2016-04-11 21:53:21 +0100

    Update and rename SelectedPaper.html to SelectedPaper.php

--- SelectedPaper.php
+++ SelectedPaper.php
@@ -10,9 +10,12 @@
 <div class="titled">
 <h1>Selected Paper</h1>
 	<div class = 'contained'>
-		<h2>Paper Title</h2>
-		<h3>Author</h3>
-		<h4>Publisher</h4>
+		<h2><?php echo $_POST['sendTitle'];?></h2>
+		
+		<h3><?php echo $_POST['sendAuthor'];?></h3>
+
+		<h4><?php echo $_POST['sendPublisher'];?></h4>
+
 		<div id="PaperIndent"><button class="button">Go to Paper</button></div>
 	</div>
 </div>
