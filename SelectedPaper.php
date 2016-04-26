3fbf648ced1b13bd2a0d614b49bf532e4f95c9f4
ChrisHutchings18 <chris-alex@hotmail.co.uk>
2016-04-26 18:53:45 +0100

    changed colour of php text

--- SelectedPaper.php
+++ SelectedPaper.php
@@ -12,13 +12,13 @@
 <div class="logo"><a href="MainPage.php"><img src="logo.png" alt="Mountain View" style="width:50px;height:50px;"></a></div><!--ADDS THE LOGO TO THE WEBSITE-->
 <h1>Selected Paper</h1>
 	<div class = 'contained'>
-		<h3>Title: <?php echo $_POST['sendTitle'];?></h3>
+		<h3>Title: <?php echo '<font color="white">'.$_POST['sendTitle'].'</font>';?></h3>
 		<table class= "summary"><tr><td><?php echo $_POST['sendSummary'];?></td></tr></table>
-		<h3>Author: <?php echo $_POST['sendAuthor'];?></h3>
+		<h3>Author: <?php echo '<font color="white">'.$_POST['sendAuthor'].'</font>';?></h3>
 
-		<h3>Publisher: <?php echo $_POST['sendPublisher'];?></h3>
-		<h3>Year Published: <?php echo $_POST['sendYear'];?></h3>
-		<h3>Citations: <?php echo $_POST['sendCites'];?></h3>
+		<h3>Publisher: <?php echo '<font color="white">'.$_POST['sendPublisher'].'</font>';?></h3>
+		<h3>Year Published: <?php echo '<font color="white">'.$_POST['sendYear'].'</font>';?></h3>
+		<h3>Citations: <?php echo '<font color="white">'.$_POST['sendCites'].'</font>';?></h3>
 
 		<div id="PaperIndent"><?php echo "<form method='get' target='_blank' action=".$_POST['sendURL']."><button type='submit'>Go to Paper</button></form>";?></div>
 	</div>
